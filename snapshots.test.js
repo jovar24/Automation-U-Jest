12dcc9f3c77d04fdf3bbd035a1fbd9dfec2fd729
Jose Vargas <jovar2407@gmail.com>
2019-11-11 20:17:43 -0700

    final test snapshot

--- snapshots.test.js
+++ snapshots.test.js
@@ -1,9 +1,9 @@
 
 var itemStock = [
-    { 'id': '1', 'itemName': 'bike', 'stock': '10' },
+    { 'id': '1', 'itemName': 'oranges', 'stock': '10' },
     { 'id': '2', 'itemName': 'monkey', 'stock': '8' },
     { 'id': '3', 'itemName': 'flour', 'stock': '13' },
-    { 'id': '4', 'itemName': 'monkey', 'stock': '2' },
+    { 'id': '4', 'itemName': 'oranges', 'stock': '6' },
     { 'id': '5', 'itemName': 'apples', 'stock': '30' },
 ]
 
@@ -15,12 +15,20 @@ function filterItemStock(arr, key, term) {
 
 test('returns all items with matching id', () => {
     expect(filterItemStock(itemStock, 'id', '1')).toEqual([
-        { 'id': '1', 'itemName': 'bike', 'stock': '10', }
+        { 'id': '1', 'itemName': 'oranges', 'stock': '10', }
     ])
 })
 test('returns all items with matching itemName', () => {
-    expect(filterItemStock(itemStock, 'itemName', 'monkey')).toBe([
-        { 'id': '2', 'itemName': 'monkey', 'stock': '8' },
-        { 'id': '4', 'itemName': 'monkey', 'stock': '2' },
+    expect(filterItemStock(itemStock, 'itemName', 'oranges')).toEqual([
+        { 'id': '1', 'itemName': 'oranges', 'stock': '10' },
+        { 'id': '4', 'itemName': 'oranges', 'stock': '6' },
+
     ])
+})
+
+test('returns all items with matching id', () => {
+    expect(filterItemStock(itemStock, 'id', '1')).toMatchSnapshot()
+})
+test('returns all items with matching itemName', () => {
+    expect(filterItemStock(itemStock, 'itemName', 'oranges')).toMatchSnapshot()
 })
